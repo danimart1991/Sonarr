00a9fb9d3dff616a32266e57bc29a152de2c310d
Daniel Martin Gonzalez <danimart1991@outlook.com>
2020-12-27 10:56:49 +0100

    Metadata Source from TMDb

---
+++ src/NzbDrone.Core/MetadataSource/TmdbSkyHook/Fanart/FanartTv.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace NzbDrone.Core.MetadataSource.TmdbSkyHook.Fanart
+{
+    public class FanartTv
+    {
+        public string Name { get; set; }
+        public string thetvdb_id { get; set; }
+        public List<FanartTvArt> clearlogo { get; set; }
+        public List<FanartTvArt> hdtvlogo { get; set; }
+        public List<FanartTvArt> clearart { get; set; }
+        public List<FanartTvSeasonArt> showbackground { get; set; }
+        public List<FanartTvArt> tvthumb { get; set; }
+        public List<FanartTvArt> seasonposter { get; set; }
+        public List<FanartTvSeasonArt> seasonthumb { get; set; }
+        public List<FanartTvArt> hdclearart { get; set; }
+        public List<FanartTvArt> tvbanner { get; set; }
+        public List<FanartTvArt> characterart { get; set; }
+        public List<FanartTvArt> tvposter { get; set; }
+        public List<FanartTvSeasonArt> seasonbanner { get; set; }
+    }
+}
