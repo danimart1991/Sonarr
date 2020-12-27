3ef374a53630d2c4873fc4a9149065c5ff623e08
Daniel Martin Gonzalez <danimart1991@outlook.com>
2020-12-27 10:56:49 +0100

    Metadata Source from TMDb

---
+++ src/NzbDrone.Core/MetadataSource/TmdbSkyHook/TmdbSkyHookException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using NzbDrone.Core.Exceptions;
+
+namespace NzbDrone.Core.MetadataSource.TmdbSkyHook
+{
+    public class TmdbSkyHookException : NzbDroneClientException
+    {
+        public TmdbSkyHookException(string message) : base(HttpStatusCode.ServiceUnavailable, message)
+        {
+        }
+
+        public TmdbSkyHookException(string message, params object[] args)
+            : base(HttpStatusCode.ServiceUnavailable, message, args)
+        {
+        }
+    }
+}
